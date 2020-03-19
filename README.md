531c6d07fac281275b56fe0b69f0afefb3701745
Gabriel Darezzo <darezzo.gabriel@gmail.com>
2020-03-19 10:22:34 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Spider to get all titles and meta-descriptions of list sites,
+
+Return a Json. 
+https://json-csv.com/
+
+
---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+set_time_limit(0);
+
+require_once('simple_html_dom.php');
+$pages = [
+	'https://br.thebar.com/johnnie-walker-oficial?uam=true&mobile=2',
+];
+
+$pageData = [];
+foreach($pages as $page) {
+	// $html = file_get_html('https://br.thebar.com/whisky-johnnie-walker-black-label--750ml-gre30259/p');
+	$html = file_get_html($page);
+	
+	
+	if($html) {
+		$pageData[] = [
+		
+			'url' => $page,
+			'title' => $html->find('title')[0]->innertext,
+			'description' => $html->find('meta[name=description]')[0]->content,
+		];
+	}
+}
+
+print json_encode($pageData);
